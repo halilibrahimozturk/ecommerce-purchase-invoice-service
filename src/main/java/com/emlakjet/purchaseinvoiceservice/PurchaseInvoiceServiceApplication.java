ab1a89f84338173544bc3fa5a65cb17617a74783
halilibrahimozturk <halilibraahimozturk@gmail.com>
2025-12-11 23:03:31 +0300

    chore: initialized project setup

---
+++ src/main/java/com/emlakjet/purchaseinvoiceservice/PurchaseInvoiceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.emlakjet.purchaseinvoiceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseInvoiceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseInvoiceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emlakjet/purchaseinvoiceservice/PurchaseInvoiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emlakjet.purchaseinvoiceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseInvoiceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
